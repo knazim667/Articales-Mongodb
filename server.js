8f2a8370e99b357cc57162708ab7e83f66797d34
Nazam <nazamzahoor@yahoo.com>
2018-10-01 11:27:27 -0700

    scrapped complete

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-//var axios = require("axios");
-//var cheerio = require("cheerio");
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 
 // initilize express
@@ -18,12 +18,47 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 //Parse Request body as JSON
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 // connect to mongo Db
 mongoose.connect("mongodb://localhost/Articles-mongo", { useNewUrlParser: true });
 
+// Routes
+
+// A get route for scrapping the newyork times website
+
+app.get('/scrape', function(req, res) {
+    axios.get('https://www.nytimes.com/section/technology').then(function(response) {
+
+        var $ =cheerio.load(response.data);
+
+
+        $("article.story").each(function(i , element) {
+
+            var result = [];
+
+            result.title = $(this)
+            .find("h2.headline")
+            .text();
+            result.summary = $(this)
+            .find("p.summary")
+            .text();
+            result.author = $(this)
+            .find("p.byline")
+            .text();
+            result.image = $(this)
+            .find("img")
+            .attr("src");
+
+            console.log(result.title);
+            console.log(result.summary);
+            console.log(result.author);
+            console.log(result.image);
+        });
+
+    });
+})
 // app.get function for main display
 app.get('/', function (req, res) {
     res.render('home');
