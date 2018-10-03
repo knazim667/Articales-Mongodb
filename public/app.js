45c4cafccd6b1d7fee6de7ba34cda0e0c6caec62
Nazam <nazamzahoor@yahoo.com>
2018-10-03 12:05:17 -0700

    design with bootstrap and make a note

--- public/app.js
+++ public/app.js
@@ -3,6 +3,71 @@ $.getJSON("/news", function(data) {
     // for each one 
     for (var i=0; i < data.length; i++) {
         // Display the information on page
-        $("#news").append("<p data-id= '" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].summary + "<br />" + data[i].author + "<br />" + data[i].image + "</p>"); 
+        $("#news").append("<div class='edit' data-id= '" + data[i]._id + "'>"
+        
+        + "<img src='"+ data[i].image +"'>"
+        + "<h3>"+ data[i].title +"</h3>"
+        + "<p>" + data[i].summary
+        + "<br/>" 
+        + "Author" + data[i].author + "</p>"+"</div>"); 
     }
 });
+
+//Whenever click on a div tag
+$(document).on("click", "div", function() {
+    // Empty the note from the Note section
+    $("#notes").empty();
+    // Save the id from the div tag
+    var thisId = $(this).attr("data-id");
+
+    // Now making ajax call from the News
+    $.ajax({
+        method: "GET",
+        url: "/news/" + thisId
+    })
+    // add the note information to the page
+    .then(function(data) {
+    
+        // The title of the article
+        $("#notes").append("<h4>" + data.title + "</h4>");
+        // An input to enter a new title
+        $("#notes").append("<input id='titleinput' name='title' >");
+        // A textarea to add a new note body
+        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+        // A button to submit a new note, with the id of the article saved to it
+        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+        //if there is a note in the news
+        if(data.note){
+            $("#titleinput").val(data.note.title);
+            $("#bodyinput").val(data.note.body);
+        }
+    });
+});
+
+// When you click the savenote button
+$(document).on("click", "#savenote", function() {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+  
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/news/" + thisId,
+      data: {
+        // Value taken from title input
+        title: $("#titleinput").val(),
+        // Value taken from note textarea
+        body: $("#bodyinput").val()
+      }
+    })
+      // With that done
+      .then(function(data) {
+        // Empty the notes section
+        $("#notes").empty();
+      });
+  
+    
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+  });
--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 //Require Dependencies
 var express = require("express");
-//var exphbs = require("express-handlebars");
+var cheerio = require('cheerio');
+var axios = require('axios');
 var bodyParser = require("body-parser");
-var logger = require("morgan");
 var mongoose = require("mongoose");
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-// Require all models
-var db = require("./models");
+var logger = require("morgan");
 
 // initilize express
 var app = express();
@@ -19,9 +15,11 @@ var PORT = 3000;
 //use morgan for logging request
 app.use(logger("dev"));
 
-// using Handlebars Models set view engine
-//app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-//app.set('view engine', 'handlebars');
+// Require all models
+var db = require("./models");
+
+
+
 app.use(express.static("public"));
 
 //Parse Request body as JSON
@@ -88,6 +86,43 @@ app.get('/news', function (req, res) {
         res.json(err);
     });
 });
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/news/:id", function(req, res) {
+    // prepare a query that finds the matching one in our db...
+    db.News.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("note")
+      .then(function(data) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(data);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+  // Route for saving/updating an Article's associated Note
+    app.post("/news/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.News.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(data) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(data);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+    });
+  
 
 // Start the server
 app.listen(PORT, function(){
